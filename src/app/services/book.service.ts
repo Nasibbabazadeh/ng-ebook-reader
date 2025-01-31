4de1c0ab03f36b6056541fdbd08b942bf9a6afaf
BabazadaN <nababazade@pasha-insurance.az>
2025-01-31 13:12:56 +0400

    feat: 1.Book service setup; 2.Generic get service setup;

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { GenericService } from './generic.service';
+import { Observable } from 'rxjs';
+import { IBook } from '../shared/models/book.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private readonly _endpoint: string = 'books';
+  constructor(private _genericService: GenericService) {}
 
-  constructor() { }
+  getAllBooks(): Observable<IBook[]> {
+    return this._genericService.get(this._endpoint);
+  }
+
+  getBookById(id: number) {
+    return this._genericService.get(`${this._endpoint}/${id}`);
+  }
+
+  searchBookByName(name: string) {
+    return this._genericService.get(`${this._endpoint}/?search=${name}`);
+  }
 }
--- src/app/services/generic.service.ts
+++ src/app/services/generic.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GenericService {
+  private readonly _baseUrl: string = 'https://gutendex.com';
 
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+
+  get(endpoint: string): Observable<any> {
+    return this._http.get(`${this._baseUrl}/${endpoint}`);
+  }
 }
